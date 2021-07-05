e99e0bfd43e16b22567fec828e9678b6dd974dd4
retiutut <retiutut@icloud.com>
2021-07-05 17:49:31 -0500

    Enabled Mouse/Touch + Keyboard Controls

--- PeacePong/Assets/PlayerControls.cs
+++ PeacePong/Assets/PlayerControls.cs
@@ -9,15 +9,19 @@ public class PlayerControls : MonoBehaviour {
 	public float speed = 30.0f;
 	public float boundY = 2.25f;
 	private Rigidbody2D rb2d;
+	private Vector3 target;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		target = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var vel = rb2d.velocity;
+
+		//Keyboard Controls
 		if (Input.GetKey (moveUp)) {
 			vel.y = speed;
 		} else if (Input.GetKey (moveDown)) {
@@ -26,6 +30,17 @@ public class PlayerControls : MonoBehaviour {
 			vel.y = 0;
 		}
 		rb2d.velocity = vel;
+
+		//Mouse/Touch Controls
+		if (Input.GetMouseButton(0))
+		{
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			//Maintain paddle X position and change Y position only (up/down)
+			target.x = transform.position.x;
+			target.z = transform.position.z;
+			transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		}
+		
 
 		var pos = transform.position;
 		if (pos.y > boundY) {
